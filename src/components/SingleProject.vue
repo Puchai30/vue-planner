3ffa4f5cf4b4bb20321aef00911d1fbe3ed0a71a
zaw <zaw@zaws-MacBook-Pro.local>
2024-06-12 00:48:10 +0700

    fist commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="single_project" :class="{ complete: single_project.complete }">
+    <div class="flexing">
+      <div>
+        <h3 @click="showDetail = !showDetail">
+          {{ single_project.title }}
+        </h3>
+      </div>
+      <div>
+        <span class="material-symbols-outlined" @click="deleteProject"> delete </span>
+        <span class="material-symbols-outlined"> edit </span>
+        <span class="material-symbols-outlined" @click="completeProject"> done </span>
+      </div>
+    </div>
+    <p v-if="showDetail">{{ single_project.detail }}</p>
+    {{ single_project.complete }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["single_project"],
+  data() {
+    return {
+      showDetail: false,
+      api: "http://localhost:3000/projects/",
+    };
+  },
+  methods: {
+    deleteProject() {
+      let deletRoute = this.api + this.single_project.id;
+      fetch(deletRoute, { method: "DELETE" })
+        .then(() => {
+          this.$emit("delete", this.single_project.id);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+
+    completeProject() {
+      let updateCompleteRoute = this.api + this.single_project.id;
+      fetch(updateCompleteRoute, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "Application/Json",
+        },
+        body: JSON.stringify({
+          complete: !this.single_project.complete,
+        }),
+      })
+        .then(() => {
+          this.$emit("complete", this.single_project.id);
+        })
+        .catch ((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+.single_project {
+  padding: 20px;
+  background-color: rgb(236, 241, 241);
+  border-left: 6px solid crimson;
+  border-radius: 8px;
+  margin: 10px;
+}
+h3 {
+  color: indigo;
+  cursor: pointer;
+}
+.flexing {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+span {
+  margin-left: 10px;
+}
+span:hover {
+  color: #777;
+  cursor: pointer;
+}
+.complete {
+  border-left-color: green;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject
+        :single_project="project"
+        @delete="deleteProj"
+        @complete="completeProj"
+      ></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleProject from "../components/SingleProject";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    SingleProject,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  methods: {
+    deleteProj(id) {
+      this.projects = this.projects.filter(project => {
+        return project.id !== id;
+      });
+    },
+
+    completeProj(id) {
+      let findProject = this.projects.find(project => {
+        return project.id == id;
+      });
+      findProject.complete =! findProject.complete;
+    },
+  },
+
+  mounted() {
+    fetch("http://localhost:3000/projects")
+      .then((response) => {
+        return response.json();
+      })
+      .then((datas) => {
+        this.projects = datas;
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  },
+};
 </script>
