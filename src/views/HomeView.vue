8e9cfe8f0eb05e1401f353621dcc1c94da8ca55d
zaw <zaw@zaws-MacBook-Pro.local>
2024-06-13 18:57:09 +0700

    filter nav with computed property

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <div v-for="project in projects" :key="project.id">
+    <FilterNav @filterValue="current=$event" :current_nav="current"></FilterNav>
+    <div v-for="project in filterdProjects" :key="project.id">
       <SingleProject
         :single_project="project"
         @delete="deleteProj"
@@ -12,16 +13,19 @@
 </template>
 
 <script>
+import FilterNav from '../components/FilterNav'
 import SingleProject from "../components/SingleProject";
 
 export default {
   name: "HomeView",
   components: {
+    FilterNav,
     SingleProject,
   },
   data() {
     return {
       projects: [],
+      current: "all"
     };
   },
   methods: {
@@ -37,6 +41,22 @@ export default {
       });
       findProject.complete =! findProject.complete;
     },
+  },
+
+  computed: {
+    filterdProjects() {
+      if(this.current==="complete"){
+        return this.projects.filter((project)=>{
+          return project.complete
+        })
+      }
+      if(this.current==="ongoing"){
+        return this.projects.filter((project)=>{
+          return !project.complete
+        })
+      }
+      return this.projects
+    }
   },
 
   mounted() {
